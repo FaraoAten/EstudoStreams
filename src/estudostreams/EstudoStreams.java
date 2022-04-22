601dc0b6e8add67c5792dffdaa493eac3030d034
FaraoAten <atenfernandes@gmail.com>
2022-04-22 17:42:22 -0300

    Segunda Aula: Operações intermediárias de streams.

--- src/estudostreams/EstudoStreams.java
+++ src/estudostreams/EstudoStreams.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class EstudoStreams {
+
     public static void main(String[] args) {
+        /*
        //Funções Lambda
        //Funções Lambda são um jeito mais simples de implementar métodos de interfaces SAM (Single Abstract Method)
        //Interfaces que tem apenas 1 método abstrato.
@@ -32,5 +34,50 @@ public class EstudoStreams {
        lista.stream()
                .filter(e -> e%2==0) //O método filter filtra os elementos presentes na stream através do que foi implementado nele com expressões Lambda.
                .forEach(e->System.out.println(e)); //O método forEach funciona como um for each;    
-    }    
+         */
+
+        //Streams parte 1 - operações intermediárias
+        List<Integer> lista = Arrays.asList(1, 5, 8, 9, 1, 4, 7, 6, 6, 9, 9);
+
+        lista.stream()
+                .skip(2) //O método skip(n) ignora os n primeiros elementos na stream.
+                .forEach(e -> System.out.println(e));
+
+        //skip é uma operação intermediária.
+        //operações intermediárias são chamadas assim porque é possível realizá-las diversas vezes antes de realizar uma operação final (inclusive uma já realizada).
+        System.out.println("\n==================================\n");
+
+        //Agora outros exemplos de operações intermediárias:
+        lista.stream()
+                .limit(2) //O método limit(n) ignora todos os outros elementos na stream após os n primeiros.
+                .forEach(e -> System.out.println(e));
+
+        System.out.println("\n==================================\n");
+
+        lista.stream()
+                .distinct() //O método distinct() ignora todos os elementos repetidos na stream, para isso ele usa o equals e o hashcode, então para utilizar esse método em uma lista de elementos autorais deve se implementar o equals e o hashcode na classe que cria esses elementos.
+                .forEach(e -> System.out.println(e));
+
+        System.out.println("\n==================================\n");
+        //Exemplo demonstrando a utilização de várias op. intermediárias em sequência:
+        lista.stream()
+                .skip(5)
+                .limit(4)
+                .distinct() //O método distinct() ignora todos os elementos repetidos na stream, para isso ele usa o equals e o hashcode, então para utilizar esse método em uma lista de elementos autorais deve se implementar o equals e o hashcode na classe que cria esses elementos.
+                .forEach(e -> System.out.println(e));
+
+        System.out.println("\n==================================\n");
+
+        lista.stream()
+                .filter(e -> e % 2 == 0) //O método filter(Predicate) permite que implementemos o filtro que quisermos.
+                .forEach(e -> System.out.println(e));
+        
+        System.out.println("\n==================================\n");
+
+        lista.stream()
+                .map(e -> e * 2) //O método map(Function) permite que implementemos uma transformação nos da stream que passam por ele (a lista original não é alterada).
+                .forEach(e -> System.out.println(e));
+        
+        //O ideal é utilizar as operções de filtragem antes das interativas.
+    }
 }
